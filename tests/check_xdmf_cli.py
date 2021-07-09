4b8c5932f53041eb6ea54e0930d717bcaef8c74f
Aaron Lentner <aaron.d.lentner@gmail.com>
2021-07-09 07:51:06 -0400

    added property based testing for cli

--- tests/check_xdmf_cli.py
+++ tests/check_xdmf_cli.py
@@ -1,13 +1,13 @@
 """Testing the cli for xdmf operation."""
 
-# type annotations
-from typing import NamedTuple
-
 # standard libraries
 import os
+from functools import partial
+from unittest.mock import patch
 
 # external libraries
 import pytest
+from hypothesis import given, strategies
 from cmdkit.app import ExitStatus 
 
 # internal libraries
@@ -16,97 +16,20 @@ from flashkit.cli.create import xdmf
 
 STATUS = ExitStatus()
 
-class Case(NamedTuple):
-    """Useful tuple of test cases definition."""
-    provided: list
-    expected: dict
+# define property testing strategies
+bools = strategies.booleans()
+naturals = strategies.integers(min_value=0)
+lists = strategies.lists(naturals, min_size=1)
+words = strategies.text(min_size=1, alphabet=strategies.characters(
+    whitelist_categories=('Lu', 'Ll', 'Nd', ), 
+    blacklist_characters=('/')))
 
-@pytest.fixture()
-def case_default():
-    """Define the default case for the xdmf cli."""
-    return Case(
-            provided=[],
-            expected={
-                'plot': None,
-                'force': None,
-                'basename': None,
-                'low': None,
-                'path': None,
-                'files': None,
-                'grid': None,
-                'ignore': False,
-                'skip': None,
-                'auto': False,
-                'find': False,
-                'high': None,
-                'dest': None,
-                'out': None
-                })
-
-@pytest.fixture()
-def case_short():
-    """Define the short option case for the xdmf cli."""
-    return Case(
-            provided=[
-                'INS_LidDr_Cavity',
-                '-b5', '-e15', '-s3','-f1,2,3,4,5',
-                '-psource', '-ddest', '-o_paraview',
-                '-c_hdf5_chk_', '-g_hdf5_geometry_', '-qforced',
-                '-I', '-A', '-B',
-                ],
-            expected={
-                'plot': '_hdf5_chk_',
-                'force': 'forced',
-                'basename': 'INS_LidDr_Cavity',
-                'low': 5,
-                'path': 'source',
-                'files': [1,2,3,4,5],
-                'grid': '_hdf5_geometry_',
-                'ignore': True,
-                'skip': 3,
-                'auto': True,
-                'find': True,
-                'high': 15,
-                'dest': 'dest',
-                'out': '_paraview',
-                })
-
-@pytest.fixture()
-def case_long():
-    """Define the long option case for the xdmf cli."""
-    return Case(
-            provided=[
-                'INS_LidDr_Cavity',
-                '--low', '5', '--high', '15', '--skip', '3', '--files', '1,2,3,4,5',
-                '--path', 'source', '--dest', 'dest', '--out', '_paraview',
-                '--plot', '_hdf5_chk_', '--grid', '_hdf5_geometry_', '--force', 'forced',
-                '--ignore', '--auto', '--find',
-                ],
-            expected={
-                'plot': '_hdf5_chk_',
-                'force': 'forced',
-                'basename': 'INS_LidDr_Cavity',
-                'low': 5,
-                'path': 'source',
-                'files': [1,2,3,4,5],
-                'grid': '_hdf5_geometry_',
-                'ignore': True,
-                'skip': 3,
-                'auto': True,
-                'find': True,
-                'high': 15,
-                'dest': 'dest',
-                'out': '_paraview',
-                })
-
-@pytest.fixture(params=[
-    pytest.lazy_fixture('case_default'),
-    pytest.lazy_fixture('case_short'),
-    pytest.lazy_fixture('case_long'),
-    ])
-def data(request):
-    """Parameterized data for testing xdmf cli."""
-    return request.param
+@pytest.fixture(scope='module')
+def mocked(module_mocker):
+    """Supporting mock facilities for cli testing."""
+    module_mocker.spy(xdmf, 'xdmf')
+    module_mocker.patch('flashkit.cli.create.xdmf.xdmf', return_value=None)
+    return module_mocker
 
 @pytest.mark.cli
 def check_xdmf_help():
@@ -134,12 +57,36 @@ def check_xdmf_badargs():
     assert STATUS.bad_argument == os.WEXITSTATUS(os.system('flashkit create xdmf -b1.2'))
 
 @pytest.mark.cli
-def check_xdmf_options(data, mocker):
-    """ Verify that the expected cli options work properly."""
+@given(basename=words, low=naturals, high=naturals, skip=naturals, files=lists,
+       path=words, dest=words, out=words, plot=words, grid=words, force=words,
+       auto=bools, find=bools, ignore=bools)
+def check_xdmf_options(basename, low, high, skip, files,path, dest, out, plot, grid, force, auto, find, ignore, mocked):
+    """Verify that the expected cli options work properly."""
+    
+    expected = {'basename': basename, 'low': low, 'high': high, 'skip': skip, 'files': files,
+                'path': path, 'dest': dest, 'out': out, 'plot': plot, 'grid': grid, 'force': force,
+                'ignore': ignore, 'auto': auto, 'find': find}
 
-    # instrument desired functions
-    mocker.patch('sys.argv', ['flashkit', 'create', 'xdmf'] + data.provided)
-    mocker.spy(xdmf, 'xdmf')
-
-    assert STATUS.runtime_error == main()
-    xdmf.xdmf.assert_called_with(**data.expected)
+    # test short form of arguments
+    _files = ','.join(str(f) for f in files)
+    _ignore = '-I ' if ignore else ''
+    _auto = '-A ' if auto else ''
+    _find = '-B ' if find else ''
+    provided = f'{basename} -b{low} -e{high} -s{skip} -f{_files} ' \
+               f'-p{path} -d{dest} -o{out} -c{plot} -g{grid} -q{force} ' \
+               f'{_ignore}{_auto}{_find}'.split()
+    with patch('sys.argv', ['flashkit', 'create', 'xdmf'] + provided):
+        assert STATUS.success == main()
+        xdmf.xdmf.assert_called_with(**expected)
+    
+    # test long form of arguments
+    _files = ','.join(str(f) for f in files)
+    _ignore = '--ignore ' if ignore else ''
+    _auto = '--auto ' if auto else ''
+    _find = '--find ' if find else ''
+    provided = f'{basename} --low {low} --high {high} --skip {skip} --files {_files} ' \
+               f'--path {path} --dest {dest} --out {out} --plot {plot} --grid {grid} --force {force} ' \
+               f'{_ignore}{_auto}{_find}'.split()
+    with patch('sys.argv', ['flashkit', 'create', 'xdmf'] + provided):
+        assert STATUS.success == main()
+        xdmf.xdmf.assert_called_with(**expected)
